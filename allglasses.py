9fc3fb5b3d6d803ea375d24df7c5db8ee3a77c7a
Eli <correcteli@gmail.com>
2020-12-13 23:06:01 -0600

    adding git ignore file

--- allglasses.py
+++ allglasses.py
@@ -4,8 +4,8 @@ import scrapy
 
 class AllglassesSpider(scrapy.Spider):
     name = 'allglasses'
-    allowed_domains = ['http://www.glassesshop.com/bestsellers']
-    start_urls = ['http://http://www.glassesshop.com/bestsellers/']
+    allowed_domains = ['www.glassesshop.com/bestsellers']
+    start_urls = ['https://http://www.glassesshop.com/bestsellers/']
 
     def parse(self, response):
         pass
